36adea48ccfdaabfa5803a5040e2f9df46d4fec9
第一次测试 <1070767978@qq.com>
2019-08-31 19:48:40 +0800

    test2

--- test.c
+++ test.c
@@ -2,8 +2,3 @@ Git is a version control system
 Git is a free distributed the GPL.
 Git is a helpful sfotware.
 Git is a abort system of us.
-<<<<<<< HEAD
-Creating a new branch is quick AND simple. 
-=======
-Creating a new branch is quick AND simple.
->>>>>>> featurel
